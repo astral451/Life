18be0e43abb28a50fed6ab361ee50bbf987cc545
Nathan Turner <nathan.turner411@gmail.com>
2019-01-01 22:07:06 -0600

    Added doc strings to most.
    Images added for Pause/Play.

--- Life/data.py
+++ Life/data.py
@@ -1,29 +1,71 @@
 
 
 class Data():
+	'''
+	The Data holds the grid and one Life object.  This is used for creation
+	plus outside UI can see the object in this way.	
+	'''
 
 	def __init__( self, div ):
 		self.grid = Grid( div )
 		self.life = Life
 
+
 	def create_life( self, x, y ):
+		'''
+		Creates a Life object at the given coordinates.  This
+		internally formats the grid to the list.	
+
+		:param x: The X coord in the grid
+		:type x: int
+		:param y: The Y coord in the grid
+		:type y: int
+		'''
 		pos = self.grid_to_pos( x, y )
 		life = self.life()
 		self.grid.add_life( pos, life )
-# 		self.grid.grid[ pos ] = life
+
 
 	def grid_to_pos( self, x, y ):
+		'''
+		Take a grid position in X,Y and convert to a linear position.
+		We have the opposite method for going back.
+
+		:param x: The X coord in the grid
+		:type x: int
+		:param y: The Y coord in the grid
+		:type y: int
+		'''
+
 		pos = y * self.grid.divisions + x
 		return pos
 
+
 	def pos_to_grid( self, pos ):
+		'''
+		Take a position in our list and find the coordinate in
+		the grid.
+			
+		:param pos: a position/index in the list
+		:type pos: int
+		'''
 		x = pos % self.grid.divisions
 		y = pos - x
 		y = y / self.grid.divisions
 
 		return x, y
 
+
 	def time_passes( self, time_delta ):
+		'''
+		This is the data update call to all the lifes.  Any of
+		the rules that need to be followed will be followed here
+		in some way
+			
+		:param time_delta: The time it took to get to this update.
+		:type time_delta: float
+		'''
+
 		idx, all_life = self.grid.get_all_life()
 		if all_life:
 			for _i, life in enumerate( all_life ):
@@ -33,16 +75,38 @@ class Data():
 
 
 class Grid():
+	'''
+	The Grid is what holds all of the lifes.  It creates/kills and 
+	knows where the lifes are.	
+	
+	:param divisions: The number of x * y is the number of entries
+	:type divisions: int
+	'''
 
 	def __init__( self, divisions ):
 		self.divisions = divisions
 		self.grid = [ None for x in range( self.divisions * self.divisions ) ]
 
-	def add_life( self, pos, life ):
+
+	def add_life( self, pos, life ):  
+		'''
+		Add the created life to a position in the list.
+			
+		:param pos: The int position in the list
+		:type pos: int
+		:param life: A Life object
+		:type life: Life
+		'''
+
 		self.grid[ pos ] = life
 		
+
 	def kill_life( self, pos ):
+		'''
+		Delete the life in this pos
+		'''
 		self.grid[ pos ] = None
+
 
 	def get_all_life( self ):
 		indexs = [ ]
@@ -57,6 +121,15 @@ class Grid():
 		else:
 			return None, None
 
+	
+	def get_neighbors_of_position( self, pos ):
+		pass
+# 		n_pos = self.get_north( pos )
+# 		ne_pos = self.get_north_east( pos )
+# 		e_pos = self.get_east( pos )
+# 		se_pos = self.get_south_east( pos )
+# 		s_pos = self.get_south( pos )
+
 	def get_north( self, pos ):
 		new_pos = pos - self.divisions
 		if new_pos >= 0:
@@ -64,12 +137,38 @@ class Grid():
 		else:
 			return None
 
+
+	def get_north_east( self, pos ):
+		pos = self.get_north( pos )
+		if pos:
+			return self.get_east( pos )
+
+
+	def get_north_west( self, pos ):
+		pos = self.get_north( pos )
+		if pos:
+			return self.get_west( pos )
+
+
 	def get_south( self, pos ):
 		new_pos = pos + self.divisions
 		if new_pos < len( self.grid ):
 			return new_pos
 		else:
 			return None
+
+
+	def get_south_east( self, pos ):
+		pos = self.get_south( pos )
+		if pos:
+			return self.get_east( pos )
+
+
+	def get_south_west( self, pos ):
+		pos = self.get_south( pos )
+		if pos:
+			return self.get_west( pos )
+
 
 	def get_west( self, pos ):
 
@@ -96,14 +195,22 @@ class Grid():
 class Life():
 	"""
 	A life-form that generates more of itself based on rules.  It also
-	has a lifespan.
+	has a lifespan.  
+	
+	TODO : I'm not sure if the life should know about neighbors, or if the 
+	grid should know.  The Grid does know about neighbors so it at least does
+	that.
 	"""
-	MAX_LIFE = 10
+	MAX_LIFE = 4 
 
 	def __init__( self ):
-		self.life = 10 
+		self.life = 4 
 
 	def decrement( self ):
+		'''
+		Decrement the life.  This doesn't account for time right now
+		just a negative	
+		'''
 		self.life -= 1
 		self.life = max( [ self.life, 0 ] )
 
--- Life/ui.py
+++ Life/ui.py
@@ -1,4 +1,11 @@
 
+"""
+The Main UI of this 'game'  It's an idea of starting/stopping the game
+of life.  https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
+
+All ui and drawing happens here.  data.py is just the Life objects and
+their grid.
+"""
 
 """
 http://zetcode.com/gui/pyqt5/firstprograms/
@@ -14,17 +21,26 @@ import PyQt5.QtWidgets as QtWidgets
 
 import data
 
+
 def get_top_app( ):
+	"""
+	Get the top window through Qt method.
+	"""
+	
 	return QtWidgets.QApplication.instance( )
 
 
 class Run_Thread( threading.Thread ):
+	"""
+	The thread for automatically updating the UI.  This is periodic, but
+	does NOT stop execution until the app closes.	
+	"""
 
 	def __init__( self, method_to_execute ):
 		super( ).__init__( )
 		self.method_to_execute = method_to_execute
 		self.keep_going = False
-		self.duration = 1
+		self.duration = 1 # duration to wait
 
 
 	def run( self ):
@@ -34,7 +50,6 @@ class Run_Thread( threading.Thread ):
 			self.method_to_execute( )
 			time.sleep( self.duration )
 
-		
 	
 	def stop( self ):
 		self.keep_going = False
@@ -42,111 +57,201 @@ class Run_Thread( threading.Thread ):
 		
 
 class App( QtWidgets.QApplication ):
+	"""
+	A top level app, but really does nothing.  I may want to delete it.
+	"""
+	
 	def __init__( self, args ):
 		super( ).__init__( args )
 		
+
 	def close( self ):
 		self.quit( )
 		
 
 
-class Window( QtWidgets.QMainWindow ):
+# Old code from examples
+# class Window( QtWidgets.QMainWindow ):
+# 	def __init__( self ):
+# 		super( ).__init__( )
+# 		
+# 		self.initUI( )
+# 		
+# 
+# 	def menu_setup( self ):
+# 		exit_act = QtWidgets.QAction( QtGui.QIcon( 'exit.png' ), '&Exit', self )
+# 		exit_act.setShortcut( 'Ctrl+Q' )
+# 		exit_act.setStatusTip( 'Exit Application' )
+# 		exit_act.triggered.connect( self._close )
+# 		
+# 		view_act = QtWidgets.QAction( 'View status bar', self, checkable = True )
+# 		view_act.setStatusTip( 'View Status bar' )
+# 		view_act.setChecked( True )
+# 		view_act.triggered.connect( self.toggle_menu )
+# 		
+# 		menu_bar = self.menuBar( )
+# 		file_menu = menu_bar.addMenu( '&File' )
+# 		file_menu.addAction( exit_act )
+# 		view_menu = menu_bar.addMenu( '&View' )
+# 		view_menu.addAction( view_act )
+# 
+# 		
+# 	def initUI( self ):
+# 		QtWidgets.QToolTip.setFont( QtGui.QFont( 'SansSerif', 10 ) )
+# 		
+# 		self.setToolTip( 'This is a <b>QWidget</b> widget' )
+# 		
+# 		btn = QtWidgets.QPushButton( 'Button', self )
+# 		btn.setToolTip( 'This is a <b>QPushButton</b> widget' )
+# 		btn.resize( btn.sizeHint( ) )
+# 		btn.move( 10, 50 )
+# 
+# 		close = QtWidgets.QPushButton( 'Quit', self )
+# 		close.clicked.connect( self._close )
+# 		close.resize( close.sizeHint( ) )
+# 		close.move( 10, 80 )
+# 		
+# 		self.setGeometry( 200, 200, 512, 512 )
+# 		self.setWindowTitle( "Life" )
+# 		self.setWindowIcon( QtGui.QIcon( '..\web.png' ) )
+# 		
+# 		self.statusbar = self.statusBar( )
+# 		self.statusbar.showMessage( 'Ready' )
+# 		self.menu_setup( )
+# 
+# 		self.show( )
+# 
+# 	def _close( self ):
+# 		event = QtGui.QCloseEvent( )
+# 		self.statusBar( ).showMessage( 'close?' )
+# 		app = get_top_app( )
+# 		app.sendEvent( app, event )
+# 
+# 		
+# 	def closeEvent( self, event ):
+# 		
+# 		reply = QtWidgets.QMessageBox.question( self, 
+# 												'Message', 
+# 												"Are you sure to quit?",
+# 												QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No,
+# 												QtWidgets.QMessageBox.No )
+# 		if reply == QtWidgets.QMessageBox.Yes:
+# 			event.accept( )
+# 		else:
+# 			event.ignore( )
+# 
+# 	
+# 	def toggle_menu( self, state ):
+# 		if state:
+# 			self.statusbar.show( )
+# 			self.statusbar.showMessage( 'Ready' )
+# 		else:
+# 			self.statusbar.hide( )
+# 		
+# 		self.update( )
+
+
+class Top_Window( QtWidgets.QMainWindow ):
+	"""
+	Top most window, contains the menus and such.
+	"""
+
 	def __init__( self ):
 		super( ).__init__( )
-		
-		self.initUI( )
-		
-
-	def menu_setup( self ):
-		exit_act = QtWidgets.QAction( QtGui.QIcon( 'exit.png' ), '&Exit', self )
-		exit_act.setShortcut( 'Ctrl+Q' )
-		exit_act.setStatusTip( 'Exit Application' )
-		exit_act.triggered.connect( self._close )
-		
-		view_act = QtWidgets.QAction( 'View status bar', self, checkable = True )
-		view_act.setStatusTip( 'View Status bar' )
-		view_act.setChecked( True )
-		view_act.triggered.connect( self.toggle_menu )
-		
-		menu_bar = self.menuBar( )
-		file_menu = menu_bar.addMenu( '&File' )
-		file_menu.addAction( exit_act )
-		view_menu = menu_bar.addMenu( '&View' )
-		view_menu.addAction( view_act )
-
-		
-	def initUI( self ):
-		QtWidgets.QToolTip.setFont( QtGui.QFont( 'SansSerif', 10 ) )
-		
-		self.setToolTip( 'This is a <b>QWidget</b> widget' )
-		
-		btn = QtWidgets.QPushButton( 'Button', self )
-		btn.setToolTip( 'This is a <b>QPushButton</b> widget' )
-		btn.resize( btn.sizeHint( ) )
-		btn.move( 10, 50 )
-		
-		close = QtWidgets.QPushButton( 'Quit', self )
-		close.clicked.connect( self._close )
-		close.resize( close.sizeHint( ) )
-		close.move( 10, 80 )
-		
 		self.setGeometry( 200, 200, 512, 512 )
 		self.setWindowTitle( "Life" )
-		self.setWindowIcon( QtGui.QIcon( '..\web.png' ) )
-		
+
+		self.grid_window = Grid_Window( )
+		self.setCentralWidget( self.grid_window )
+
 		self.statusbar = self.statusBar( )
-		self.statusbar.showMessage( 'Ready' )
+		self.statusbar.showMessage( 'Paused' )
 		self.menu_setup( )
 
-		self.show( )
 
-	def _close( self ):
-		event = QtGui.QCloseEvent( )
-		self.statusBar( ).showMessage( 'close?' )
-		app = get_top_app( )
-		app.sendEvent( app, event )
-
+	def menu_setup( self ):
+		"""
+		The menu for Pause and Play	
+		"""
+		play_act = QtWidgets.QAction( QtGui.QIcon( '.\_images\play.png' ), '&Play', self )
+		play_act.setShortcut( 'Ctrl+P' )
+		play_act.setStatusTip( 'Play' )
+		play_act.triggered.connect( self._play )
 		
+		pause_act = QtWidgets.QAction( QtGui.QIcon( '.\_images\pause.png' ), 'P&ause', self )
+		pause_act.setShortcut( 'Ctrl+A' )
+		pause_act.setStatusTip( 'Pause' )
+		pause_act.triggered.connect( self._pause )
+		
+		menu_bar = self.menuBar( )
+		menu_bar.addAction( play_act )
+		menu_bar.addAction( pause_act )
+
+
+	def _pause( self ):
+		"""
+		sets the grids thread to pause, or rather to just stop time_passes 
+		"""
+		self.statusbar.showMessage( 'Pause' )
+		self.grid_window.run = False
+		
+		
+	def _play( self ):
+		"""
+		sets the grids thread to play, or rather to just start time_passes 
+		"""
+		self.statusbar.showMessage( 'Play' )
+		self.grid_window.run = True 
+
+
 	def closeEvent( self, event ):
-		
+		"""
+		Controls the close event, prompting the user if they want to close.
+		This also stops the running Thread if the user says Yes.
+		"""
 		reply = QtWidgets.QMessageBox.question( self, 
 												'Message', 
 												"Are you sure to quit?",
 												QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No,
 												QtWidgets.QMessageBox.No )
 		if reply == QtWidgets.QMessageBox.Yes:
+			self.grid_window.thread.stop( )
 			event.accept( )
 		else:
 			event.ignore( )
 
-	
-	def toggle_menu( self, state ):
-		if state:
-			self.statusbar.show( )
-			self.statusbar.showMessage( 'Ready' )
-		else:
-			self.statusbar.hide( )
-		
-# 		self.update( )
 
 
-class Top_Window( QtWidgets.QWidget ):
+class Grid_Window( QtWidgets.QWidget ):
+	"""
+	This window holds the Grid. This layer also holds the thread
+	that is running. The widget is designed to keep updating the UI
+	as it goes.  The grid itself is what draws however.	
+	"""
 	def __init__( self ):
 		super( ).__init__( )
 		
-		self.setWindowTitle( "Life" )
-		self.setGeometry( 200, 200, 512, 512 )
 		layout = QtWidgets.QGridLayout( )
 		self.grid = Grid( )
 		layout.addWidget( self.grid )
 		
+
+		self.run = False
 		self.thread = Run_Thread( self.on_thread_update )
 		
 		self.setLayout( layout )
 		self.thread.start( )
 	
 
+
 	def on_thread_update( self ):
+		"""
+		The method being executed by our Thread object.  This is calling
+		the UI update every {duration} from the thread.  All it does is
+		create a PaintEvent and send it to the top level window and that
+		cascades into the rest of the UI.  The only additional call is to
+		the grid, and for it to update ( redraw in qt )
+		"""
 		self.time_passes( )
 
 		paint_event = QtGui.QPaintEvent( self.rect( ) )
@@ -156,6 +261,19 @@ class Top_Window( QtWidgets.QWidget ):
 
 	
 	def time_passes( self ):
+		"""
+		Time passes is method to calculate the duration of the wait
+		and to call a time_passes on the underlying data.  This is
+		coming from the UI so the Data is responsible for knowing
+		what to do with the duration.  
+		
+		:**TODO**:
+			* A flaw in this code is that we are tracking the delta even while paused.
+			I think this is possibly going to cause problems when the Lifes 
+			are listening for real duration.  I'll need to find away to pause
+			the advancement of time, not simply stop processing.
+
+		"""
 		current_time = time.clock( )
 		if not hasattr( self, '_start_time' ):
 			self._start_time = current_time 
@@ -163,26 +281,19 @@ class Top_Window( QtWidgets.QWidget ):
 		# update our lifes
 		delta = current_time - self._start_time
 		self._start_time = current_time
-		self.grid.data.time_passes( delta )
+		if self.run:
+			self.grid.data.time_passes( delta )
 
 	
-	def closeEvent( self, event ):
-		
-		reply = QtWidgets.QMessageBox.question( self, 
-												'Message', 
-												"Are you sure to quit?",
-												QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No,
-												QtWidgets.QMessageBox.No )
-		if reply == QtWidgets.QMessageBox.Yes:
-			self.thread.stop( )
-			event.accept( )
-		else:
-			event.ignore( )
 
 
 	
 
 class Grid( QtWidgets.QWidget ):
+	"""
+	The grid is drawing the grid and any lifes that exist.  It does handle
+	all visual updates.	
+	"""
 	def __init__( self ):
 		super( ).__init__( )
 		
@@ -190,13 +301,18 @@ class Grid( QtWidgets.QWidget ):
 		font.setPixelSize( 10 )	
 		self.setFont( font )
 		
-		self.grid_size = 10 
+		# TODO : Need to externalize this
+		self.grid_size = 20 
 		self.data = data.Data( self.grid_size )
 		
 		self.setMouseTracking( True )
 		
 
 	def mousePressEvent(self, event ):
+		"""
+		Mouse events for clicking around the grid.  It does the math
+		to figure out what grid square you are in.	
+		"""
 		x = event.x( )
 		y = event.y( )
 		
@@ -209,10 +325,19 @@ class Grid( QtWidgets.QWidget ):
 
 		self.data.create_life( test_x, test_y )
 		
+		# pass the mouse event along.
 		return QtWidgets.QWidget.mousePressEvent(self, event )
 
 
 	def draw_life( self, painter ):
+		'''
+		Draw the life, start by figuring out where it is in the grid. There
+		is a sub-call to draw the square, but this is the starting point.
+				
+		:param painter:
+		:type painter:
+		'''
+
 
 		idxs, lifes = self.data.grid.get_all_life( )
 
@@ -225,8 +350,8 @@ class Grid( QtWidgets.QWidget ):
 				x, y = self.data.pos_to_grid( l_idx )
 				pos_x = int( x * subdiv_x ) - 1 # TODO, better alignment
 				pos_y = int( y * subdiv_y ) - 1
-				end_x = int( pos_x + subdiv_x )
-				end_y = int( pos_y + subdiv_y )
+				end_x = int( pos_x + subdiv_x ) - 1
+				end_y = int( pos_y + subdiv_y ) - 1
 
 				start_point = QtCore.QPoint( pos_x, pos_y )
 				end_point = QtCore.QPoint( end_x, end_y )
@@ -235,8 +360,8 @@ class Grid( QtWidgets.QWidget ):
 
 	
 	def draw_square( self, painter, start_point, end_point, life ):
-		cur_val = life.life
-		# clamp the value, just in case
+		# turn value into a color and clamp the value, just in case
+		# Note I invert the value so it goes from black -> white
 		life_color = max( 0 , min( 255, 255 * ( ( life.MAX_LIFE - life.life ) / life.MAX_LIFE ) ) )
 
 
